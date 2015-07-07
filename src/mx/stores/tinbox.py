1b7375c3e801a47f4ff2180662bc82c0611f1b0b
adisbladis <adis@blad.is>
2015-07-07 14:23:41 +0800

    Add attachments

--- src/mx/stores/tinbox.py
+++ src/mx/stores/tinbox.py
@@ -17,17 +17,22 @@ def insert(mail):
 
         body_content = mail.get_body_content()
 
+        attachments = list(mail.get_attachments())
+
         uuids = []
 
         for match in re.finditer(r'[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}',
                                  body_content):
             uuids.append(match.group(0))
 
-        tinbox.create_ticket(email, mail.subject, mail.get_body_content(),
-                             sender_name=name,
-                             context=uuids or None)
+        resp = tinbox.create_ticket(
+            email, mail.subject, mail.get_body_content(),
+            sender_name=name, context=uuids or None,
+            attachments=[a.filename for a in attachments])
+
+        for attachment_pk, attachment in zip(resp['attachments'], attachments):
+            tinbox.upload_attachment(attachment_pk, attachment.data)
 
     except Exception as e:
         _log.exception('Could not insert into trak.')
         raise BackendError(e)
-
