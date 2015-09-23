fe31556413850ac367081519d671b9d60b9d9560
Joar Wandborg <joar@wandborg.se>
2015-09-23 17:19:42 +0200

    Log exception when mail insert fails

--- src/mx/cli/command.py
+++ src/mx/cli/command.py
@@ -129,17 +129,17 @@ class Interface(object):
                     #       CLI option --delete?
 
                 except BackendError:
-                    logger.error('Failed to import mail: %s', uid)
+                    logger.exception('Failed to import mail: %s', uid)
                     client.mark_unseen(index)
                     # TODO: Catch correct api exception
                     # TODO: Handle failed api call, mark as unseen. Flag with
                     #       try-count? Delete after X tries?
 
                 except Exception:
-                    logger.error('Failed to parse mail: %s', uid)
+                    logger.exception('Failed to parse mail: %s', uid)
                     # TODO: Create custom parse exception
                     # TODO: Handle mail parse error. Move to other mailbox?
-                    #       eave as seen?
+                    #       leave as seen?
 
     @property
     def imap_settings(self):
